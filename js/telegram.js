6f7f76fe2b8a5eb7b27807168488e565b8d5ea2e
邓嘉军 <dengjiajun@dengjiajundeMacBook-Pro.local>
2024-06-05 00:37:31 +0800

    up data

--- js/telegram.js
+++ js/telegram.js
@@ -1,5 +1,33 @@
+function getUserInfo() {
+    const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    if (user) {
+      // 如果用户信息存在，则返回用户信息
+      return {
+        query_id: initDataUnsafe.query_id,
+        user_id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        allows_write_to_pm: user.allows_write_to_pm,
+        hash: initDataUnsafe.hash,
+        auth_date: initDataUnsafe.auth_date
+      };
+    } else {
+      // 如果用户信息不存在，则返回未登录信息
+      return {
+        query_id: null,
+        user_id: null,
+        first_name: null,
+        last_name: null,
+        allows_write_to_pm: null,
+        hash: null,
+        auth_date: null
+      };
+    }
+  }
 
-(function() {
+
+( function() {
     onload = function () {
         // 获取用户信息并显示
         const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
