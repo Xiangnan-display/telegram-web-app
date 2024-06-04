56f9a1b98a8b7d70385538ec9a445aeb768687e8
邓嘉军 <dengjiajun@dengjiajundeMacBook-Pro.local>
2024-06-05 03:20:25 +0800

    up data

--- js/request.js
+++ js/request.js
@@ -8,8 +8,38 @@ for (let i = 0; i < urlkeyword.length; i++) {
     var value = item[1]
     axiosParam[key] = value
 }
+
+//消耗当天挑战次数
 var consumeC = function(){
     axios.get('http://herometa.io/web-hook/consume/challenge?userId=' + axiosParam.telegramUserId, {
+        headers: { 'token': axiosParam.token },
+        withCredentials: false
+    })
+        .then(res => {
+            //alert(0)
+        })
+        .catch(error => {
+            //alert(error)
+        })
+}
+
+//查询当天挑战次数
+var getChallengeCount = function(){
+    axios.get('http://herometa.io//web-hook/find/challenge?userId=' + axiosParam.telegramUserId, {
+        headers: { 'token': axiosParam.token },
+        withCredentials: false
+    })
+        .then(res => {
+            //alert(0)
+        })
+        .catch(error => {
+            //alert(error)
+        })
+}
+
+//查询积分
+var getPoints = function(){
+    axios.get('http://herometa.io/web-hook/find/integration?userId=' + axiosParam.telegramUserId, {
         headers: { 'token': axiosParam.token },
         withCredentials: false
     })
