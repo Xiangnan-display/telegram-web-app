5301c8fec279c082282b1cb4b3f9701fea6964b3
邓嘉军 <dengjiajun@dengjiajundeMacBook-Pro.local>
2024-06-05 01:08:58 +0800

    up data

--- js/telegram.js
+++ js/telegram.js
@@ -26,6 +26,91 @@
 //     }
 //   }
 
+// function getUserInfo() {
+//     const userInfoString = localStorage.getItem('userInfo');
+//     if (userInfoString) {
+//         try {
+//             return JSON.parse(userInfoString);
+//         } catch (error) {
+//             console.error("Error parsing user info from localStorage", error);
+//             return null;
+//         }
+//     }
+//     return null;
+// }
+
+(function() {
+    window.onload = function() {
+        // try {
+        //     const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+        //     const user = initDataUnsafe.user;
+        //     if (user && user.id) {
+        //         let userInfo = {
+        //             query_id: initDataUnsafe.query_id,
+        //             user_id: user.id,
+        //             first_name: user.first_name,
+        //             last_name: user.last_name,
+        //             allows_write_to_pm: user.allows_write_to_pm,
+        //             hash: initDataUnsafe.hash,
+        //             auth_date: initDataUnsafe.auth_date
+        //         };
+
+        //         alert("用户名称：" + user.first_name);
+
+        //         // 将用户信息转换为 JSON 字符串以便存储
+        //         const userInfoString = JSON.stringify(userInfo);
+                
+        //         // 存储到 localStorage
+        //         localStorage.setItem('userInfo', userInfoString);
+        //     } else {
+        //         // 如果用户信息不存在，则显示未登录信息
+        //         alert("未登录授权信息");
+        //     }
+        // } catch (error) {
+        //     console.error("Error initializing user data", error);
+        //     alert("初始化用户数据时出错");
+        // }
+        try {
+            // 初始化 Telegram Web App
+             Telegram.WebApp.ready();
+    
+            const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+            const user = initDataUnsafe.user;
+    
+            if (user && user.id) {
+                let userInfo = {
+                    query_id: initDataUnsafe.query_id,
+                    user_id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    allows_write_to_pm: user.allows_write_to_pm,
+                    hash: initDataUnsafe.hash,
+                    auth_date: initDataUnsafe.auth_date
+                };
+    
+                // 将用户信息转换为 JSON 字符串以便存储
+                const userInfoString = JSON.stringify(userInfo);
+                
+                // 存储到 localStorage
+                localStorage.setItem('userInfo', userInfoString);
+            } else {
+                // 如果用户信息不存在，则显示未登录信息
+                alert("未登录授权信息1112222");
+            }
+        } catch (error) {
+            console.error("Error initializing user data", error);
+            alert("初始化用户数据时出错");
+        }
+    };
+})();
+
+
+// 获取并存储用户信息
+async function fetchAndStoreUserInfo() {
+   
+}
+
+// 从本地存储获取用户信息
 function getUserInfo() {
     const userInfoString = localStorage.getItem('userInfo');
     if (userInfoString) {
@@ -38,37 +123,3 @@ function getUserInfo() {
     }
     return null;
 }
-
-(function() {
-    window.onload = function() {
-        try {
-            const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-            const user = initDataUnsafe.user;
-            if (user && user.id) {
-                let userInfo = {
-                    query_id: initDataUnsafe.query_id,
-                    user_id: user.id,
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    allows_write_to_pm: user.allows_write_to_pm,
-                    hash: initDataUnsafe.hash,
-                    auth_date: initDataUnsafe.auth_date
-                };
-
-                alert("用户名称：" + user.first_name);
-
-                // 将用户信息转换为 JSON 字符串以便存储
-                const userInfoString = JSON.stringify(userInfo);
-                
-                // 存储到 localStorage
-                localStorage.setItem('userInfo', userInfoString);
-            } else {
-                // 如果用户信息不存在，则显示未登录信息
-                alert("未登录授权信息");
-            }
-        } catch (error) {
-            console.error("Error initializing user data", error);
-            alert("初始化用户数据时出错");
-        }
-    };
-})();
