339518e7512f1124df7d61d984411b18f25a2e8c
邓嘉军 <dengjiajun@dengjiajundeMacBook-Pro.local>
2024-06-05 00:51:12 +0800

    up data

--- js/telegram.js
+++ js/telegram.js
@@ -1,39 +1,60 @@
-function getUserInfo() {
-    const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-    const user = window.Telegram.WebApp.initDataUnsafe.user;
-    if (user) {
-      // 如果用户信息存在，则返回用户信息
-      return {
-        query_id: initDataUnsafe.query_id,
-        user_id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        allows_write_to_pm: user.allows_write_to_pm,
-        hash: initDataUnsafe.hash,
-        auth_date: initDataUnsafe.auth_date
-      };
-    } else {
-      // 如果用户信息不存在，则返回未登录信息
-      return {
-        query_id: null,
-        user_id: null,
-        first_name: null,
-        last_name: null,
-        allows_write_to_pm: null,
-        hash: null,
-        auth_date: null
-      };
-    }
-  }
+// function getUserInfo() {
+//     const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+//     const user = window.Telegram.WebApp.initDataUnsafe.user;
+//     if (user) {
+//       // 如果用户信息存在，则返回用户信息
+//       return {
+//         query_id: initDataUnsafe.query_id,
+//         user_id: user.id,
+//         first_name: user.first_name,
+//         last_name: user.last_name,
+//         allows_write_to_pm: user.allows_write_to_pm,
+//         hash: initDataUnsafe.hash,
+//         auth_date: initDataUnsafe.auth_date
+//       };
+//     } else {
+//       // 如果用户信息不存在，则返回未登录信息
+//       return {
+//         query_id: null,
+//         user_id: null,
+//         first_name: null,
+//         last_name: null,
+//         allows_write_to_pm: null,
+//         hash: null,
+//         auth_date: null
+//       };
+//     }
+//   }
 
+  function getUserInfo(){
+    const userInfoString = localStorage.getItem('userInfo');  
+    if (userInfoString) {  
+        return JSON.parse(userInfoString);  
+    }  
+    return null;  
+    }
 
 ( function() {
     onload = function () {
         // 获取用户信息并显示
         const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
         const user = window.Telegram.WebApp.initDataUnsafe.user;
-        if (user) {
-            alert("用户名称："+user.first_name)
+        let userInfo =  {
+                    query_id: initDataUnsafe.query_id,
+                    user_id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    allows_write_to_pm: user.allows_write_to_pm,
+                    hash: initDataUnsafe.hash,
+                    auth_date: initDataUnsafe.auth_date
+                  };
+        if (userInfo.user_id) {
+            alert("用户名称2："+user.first_name)
+            // 将用户信息转换为 JSON 字符串以便存储  
+            const userInfoString = JSON.stringify(userInfo);  
+                
+            // 存储到 localStorage  
+            localStorage.setItem('userInfo', userInfoString);  
             // 如果用户信息存在，则显示用户信息
             // const userInfoElement = document.getElementById('user-info');
             // userInfoElement.innerHTML = `
@@ -50,7 +71,7 @@ function getUserInfo() {
             // `;
         } else {
             // 如果用户信息不存在，则显示未登录信息
-            alert("未登录授权信息")
+            alert("未登录授权信息2")
         }
     };
 })();
